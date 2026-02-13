397b40b2e3031be4be0a177b4c86b587eb7c79cd
svecomar-alt <svecomar@gmail.com>
2026-02-13 10:27:55 +0100

    lekce

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 alert("Zkouška")
 alert(3)
 
-let username = "Adélka"
+let username = "Adélka";
 console.log(userName);
 
-userName = "Honza"
-console.log(userName)
+userName = "Honza";
+console.log(userName);
 
 console.log(7+4);
 console.log("7" + "4");
 console.log("7" + 4);
+
+let age = 17;
+console.log(age > 20)
+
+let text = "Text"; 
+console.log(typeof text)
+let hodnota = null;
+
+let cisloA = 10;
+let cisloB = 5;
+console.log (cisloA + cisloB)
+
+// Umocňování ** 
+console.assert.log(cisloA ** cisloB)
+// Zbytek 
+console.log(cisloA % cisloB)
+
+// Aktualizace proměnné
+let number = 18;
+number = number + 1;
+
+number+=1;
+number-=2;
+number+=4;
+number/=6;
+number%= 3;
+number++;
+number--;
+
