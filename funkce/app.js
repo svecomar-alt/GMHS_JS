d1b5a02d239e606d081088fed70f696fe3525eb7
svecomar-alt <svecomar@gmail.com>
2026-05-29 10:30:44 +0200

    lekcicka

---
+++ funkce/app.js
@@ -0,0 +1,47 @@
+function rekniAhoj()  {
+    console.log("Čus")
+}
+ rekniAhoj();
+ rekniAhoj();
+
+ let pozdrav = "Pozdrav!";
+
+function ukazPozdrav {
+    console.log(pozdrav)
+}
+
+function testLocal() {
+    let localValue = 123;
+    console.log(localValue)
+}
+
+ukazPozdrav();
+testLocal();
+console.log(localValue);
+ if (true) {
+    let x = "jen unvnitř bloku";
+
+ }
+ console.log(x);
+
+ if(true) {
+    var y = "var blok nerespektuje";
+ }
+console.log(y);
+ // Parametr a Argument
+
+ function sayHello(name) { // name = parametr
+     console.log("Ahoj" + name);
+ }
+       sayHello(name: any) void
+sayHello("Marie")
+function addNumber(num1, num2) {
+    console,log(num1+num2);
+
+}
+
+addNumber(6,7);
+
+// Napiš funkci printRange, která vypíše do konzole všechna čísla od paramentru start až po end
+
+funcion 
